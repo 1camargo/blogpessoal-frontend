0521939e5fb3ccbbd0b50a1fa5da9f3d1f0ef208
Gabriel Camargo <camargoxg@gmail.com>
2024-08-07 18:32:33 -0300

    :sparkles: feat: creating routes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import Home from './pages/Home/Home';
+import React from 'react';
 import './App.css';
-
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-   <Home />
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
 export default App;
